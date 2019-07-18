642e9c0527a18c75b8201398ef1bc82bc45265ad
Aleem Isiaka <aleemisiaka@gmail.com>
2019-07-18 14:21:00 +0100

    feat: inializing

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,30 @@
-import babel from "rollup-plugin-babel";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import { uglify } from "rollup-plugin-uglify";
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
 
 const umdGlobals = {
-  react: "React",
-  "prop-types": "PropTypes"
+  react: 'React',
+  'prop-types': 'PropTypes',
 };
 
 export default {
-  input: "./src/main.js",
+  input: './src/index.js',
   output: {
-    file: "dist/index.js",
-    format: "umd",
-    name: "reactVideoTrimmer",
+    file: 'dist/index.js',
+    format: 'umd',
+    name: 'reactVideoTrimmer',
     globals: umdGlobals,
-    sourcemap: "inline",
-    exports: "named"
+    sourcemap: 'inline',
+    exports: 'named',
   },
   external: Object.keys(umdGlobals),
   plugins: [
     babel({
-      exclude: "node_modules/**"
+      exclude: 'node_modules/**',
     }),
     resolve(),
     commonjs(),
-    uglify()
-  ]
+    uglify(),
+  ],
 };
---
+++ styleguide.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const { createConfig, babel, css } = require('webpack-blocks');
+
+module.exports = {
+  title: 'react-video-trimmer',
+  styleguideDir: path.join(__dirname, 'styleguide'),
+  webpackConfig: createConfig([babel(), css()]),
+  exampleMode: 'expand',
+  usageMode: 'expand',
+  showSidebar: false,
+  serverPort: 8080,
+  moduleAliases: {
+    'react-video-trimmer': path.resolve(__dirname, './src'),
+  },
+};
