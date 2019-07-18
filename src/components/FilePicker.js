c9db527191f5227bc0a21afa3fe6d9a8faf72712
Aleem Isiaka <aleemisiaka@gmail.com>
2019-07-18 21:10:00 +0100

    feat: add utils.js

---
+++ src/components/FilePicker.js
@@ -0,0 +1,21 @@
+import React, { useCallback } from "react";
+import { isVideo, noop } from "../libs/utils";
+
+function FilePicker(props) {
+  const handleFileChange = useCallback(e => {
+    if (e.target.files.length) {
+      const video = e.target.files[0];
+      if (isVideo(video)) {
+        const handler = props.onFileSelected || noop;
+        handler(file);
+      }
+    }
+  });
+  return (
+    <div>
+      <input type="file" accept="video/*" onChange={handleFileChange} />
+    </div>
+  );
+}
+
+export default FilePicker;
--- src/components/FilePicker.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function FilePicker() {
-  return <div>This is the file picker</div>;
-}
-
-export default FilePicker;
