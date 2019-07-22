5b98a39796aad0f053b9138e8891bf78e815cc28
Aleem Isiaka <aleemisiaka@gmail.com>
2019-07-22 20:54:45 +0100

    fix: package.json redunt modules

--- src/libs/workerSetup.js
+++
@@ -1,9 +0,0 @@
-export default class WebWorker {
-  constructor(worker) {
-    const code = worker.toString();
-    const blob = new Blob(["(" + code + ")()"], {
-      type: "application/javascript"
-    });
-    return new Worker(URL.createObjectURL(blob));
-  }
-}
